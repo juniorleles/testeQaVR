f1627e5977b31f4e748935b4e937235d6d54da4f
joao.serafim <joao.serafim@ltm.digital>
2019-07-30 09:43:15 -0300

    add tudo

--- features/specifications/buscarCep.feature
+++ features/specifications/buscarCep.feature
@@ -2,12 +2,10 @@
 @buscarCep
 Funcionalidade: Consultar os dados de um endereço a partir de um CEP
 
-Cenário: Buscar um endereço a partir de um CEP
-    Dado o endereço da api para buscar os dados de um endereço
-    Quando realizar uma requisição para buscar um endereço 
-    Então a API deve retornar os dados do endereço respondendo o código 200
+Cenário: Buscar um endereço a partir de um CEP válido
+    Quando realizar uma requisição para buscar um CEP válido
+    Então a API deve retornar os dados do CEP respondendo com o código 200
 
-Cenário: Buscar um endereço a partir de um CEP Invalido
-    Dado o endereço da api para buscar os dados de um endereço
-    Quando realizar uma requisição para buscar um endereço 
-    Então a API deve retornar os dados com erro o código 200
+Cenário: Buscar um endereço a partir de um CEP Inválido
+    Quando realizar uma requisição para buscar um CEP Inválido 
+    Então a API deve retornar os dados de endereço como inválido
--- features/step_definitions/cep_steps.rb
+++ features/step_definitions/cep_steps.rb
@@ -1,30 +1,26 @@
-Dado("o endereço da api para buscar os dados de um endereço") do
-    
-  end
-  
-  Quando("realizar uma requisição para buscar um endereço") do
+
+  Quando("realizar uma requisição para buscar um CEP válido") do
     $response = HTTParty.get('https://viacep.com.br/ws/05544150/json/')
   end
   
-  Então("a API deve retornar os dados do endereço respondendo o código {int}") do |int|
+  
+  Então("a API deve retornar os dados do CEP respondendo com o código {int}") do |int|
     puts  "Response code: #{$response.code}"
     expect($response.code).to eq(200)
-    puts "Response body: #{$response.body}"
     puts  "IBGE:       #{$response['ibge']}"
-    
-  end
 
-  Dado("o endereço da api para buscar os dados de um endereço invalido") do
-   
   end
-  
-  Quando("realizar uma requisição para buscar um endereço invalido") do
+    
+
+  Quando("realizar uma requisição para buscar um CEP Inválido") do
     $response = HTTParty.get('https://viacep.com.br/ws/05544560/json/')
   end
   
-  Então("a API deve retornar os dados com erro o código {int}") do |int|
+ 
+  Então("a API deve retornar os dados de endereço como inválido") do
     expect($response.code).to eq(200)
     puts  "Status code:#{$response.code}"
+    
   
   end
 
